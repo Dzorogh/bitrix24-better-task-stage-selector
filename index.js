1174d03f52206bf28f3fc65e57f91df01b58701e
Сергей Инденбом <dzorogh@gmail.com>
2024-02-22 12:45:43 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Bitirix24 Stage Selector
+// @namespace    https://crm.globaldrive.ru/
+// @version      2024-02-22
+// @description  Better Tasks Stage Selector
+// @author       Dzorogh
+// @match        https://crm.globaldrive.ru/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const css = `
+div#tasksStagesWrap {
+  height: auto;
+}
+
+div#tasksStages {
+  height: auto;
+  flex-direction: column;
+  position: inherit;
+  background-color: transparent !important;
+}
+
+div#tasksStages .task-section-status-step {
+  font-size: 12px;
+  padding: 0.5em 1em;
+  border: 0 !important;
+  border-radius: 0 !important;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: #000 !important;
+}
+
+div#tasksStages .task-section-status-step:hover {
+  font-weight: bold;
+}
+    `;
+
+    GM_addStyle(css);
+
+    console.log('Bitirix24 Stage Selector: Done');
+
+})();
